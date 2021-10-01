7b81b64931096167a6a2618a5bf6c3c160acc3a2
AntonBush <bushev.anton.ru@yandex.ru>
2021-10-01 18:57:05 +0300

    Heroku preparing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    id("application")
     id("org.springframework.boot") version "2.5.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.31"
